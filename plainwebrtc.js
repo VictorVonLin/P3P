e42c8adcb7fd0fc853f0d2fb32fedc80caa73c6b
Victor Lin <50209221+VictorVonLin@users.noreply.github.com>
2023-03-16 00:00:08 -0700

    Testing Turn Servers...

--- plainwebrtc.js
+++ plainwebrtc.js
@@ -1,5 +1,27 @@
 var url = "https://script.google.com/macros/s/AKfycbz2guCH2a52rLP2iTYDeUMwZXiwe09rasTzufLNH6Ijw6JkHMtPxFlSBODptB-EGlWn/exec?callback"
-var conf = {iceServers: [{urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19305" ]}]};
+// var conf = {iceServers: [{urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19305" ]}]};
+var conf = {
+	 iceServers: [
+      {
+        urls: "stun:relay.metered.ca:80",
+      },
+      {
+        urls: "turn:relay.metered.ca:80",
+        username: "0ed15aa73fad92108208a1ac",
+        credential: "gbTVHChOgU34H1II",
+      },
+      {
+        urls: "turn:relay.metered.ca:443",
+        username: "0ed15aa73fad92108208a1ac",
+        credential: "gbTVHChOgU34H1II",
+      },
+      {
+        urls: "turn:relay.metered.ca:443?transport=tcp",
+        username: "0ed15aa73fad92108208a1ac",
+        credential: "gbTVHChOgU34H1II",
+      },
+  ],
+};
 var pc = new RTCPeerConnection(conf);
 var	context, _chatChannel; 
 var protocol = true; //true for offer, false for answer
